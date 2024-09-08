c083748d49b16b6bdee06abf633a9e7b241223b7
Amanda Maree Jones <amandamareejones@gmail.com>
2024-09-08 15:46:48 +1000

    CSS - style added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function showAnswer(response) {
+function handleClick(response) {
   alert(response.data.answer);
 }
 let apiKey = "09te212a1c952dfb01916b44eoad6c23";
@@ -6,5 +6,6 @@ let context =
   "You are an intelligent AI Assistant, who provides clear and concise answers. Provide only one sentance";
 let prompt = "Who was the first female president?";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-axios.get(apiUrl).then(showAnswer);
+let buttonElement = document.querySelector("#special-button");
+buttonElement.addEventListener("click", handleClick);
+axios.get(apiUrl).then(handleClick);
